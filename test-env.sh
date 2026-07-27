758ee43182e21051c9298e52edea6abe732c6199
Francis Belanger <francis.belanger@gmail.com>
2026-07-27 06:52:33 -0400

    chore: remove unneeded files

--- test-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "=== Environment Test ==="
-echo "HOLLOW_TRANSPORT=${HOLLOW_TRANSPORT:-NOT_SET}"
-echo "HOLLOW_COMMAND_ADDR=${HOLLOW_COMMAND_ADDR:-NOT_SET}"
-echo "HOLLOW_PANE_ID=${HOLLOW_PANE_ID:-NOT_SET}"
-echo "WSLENV=${WSLENV:-NOT_SET}"
-echo "======================="
